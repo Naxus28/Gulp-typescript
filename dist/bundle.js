cebaae73933b549d9c65a39d9c02971f10444210
Gabriel Ferraz <gferraz@fullsail.com>
2017-03-21 15:35:17 -0400

    implements babel and babelify transforms; changes 'tsconfig.json' 'target: es2015'

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,2 +1,23 @@
-!function e(r,t,n){function o(i,f){if(!t[i]){if(!r[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var l=new Error("Cannot find module '"+i+"'");throw l.code="MODULE_NOT_FOUND",l}var a=t[i]={exports:{}};r[i][0].call(a.exports,function(e){var t=r[i][1][e];return o(t?t:e)},a,a.exports,e,r,t,n)}return t[i].exports}for(var u="function"==typeof require&&require,i=0;i<n.length;i++)o(n[i]);return o}({1:[function(e,r,t){"use strict";function n(e){return"Hello from "+e}Object.defineProperty(t,"__esModule",{value:!0}),t.sayHello=n},{}],2:[function(e,r,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=e("./greet");!function(e,r){document.getElementById(e).innerText=n.sayHello(r)}("greeting","TypeScript")},{"./greet":1}]},{},[2]);
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+function sayHello(name) {
+    return "Hello from " + name;
+}
+exports.sayHello = sayHello;
+
+},{}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var greet_1 = require("./greet");
+function showHello(divName, name) {
+    var elt = document.getElementById(divName);
+    elt.innerText = greet_1.sayHello(name);
+}
+showHello("greeting", "TypeScript");
+
+},{"./greet":1}]},{},[2])
+
 //# sourceMappingURL=bundle.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
 var tsify = require("tsify");
-var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
 var paths = {
@@ -28,12 +27,14 @@ gulp.task("browserify", function() {
 
   return browserify(browserifyConfig)
   .plugin(tsify) // browserify plugin to compile typescript
+   .transform('babelify', {
+      presets: ['es2015'],
+      extensions: ['.ts']
+  })
   .bundle()
-    .on('error', function (err) { console.error(err); })
   .pipe(source('bundle.js')) // in memory bundled file
   .pipe(buffer()) // need to buffer to be able to use other js plugins
   .pipe(sourcemaps.init({loadMaps: true})) // this will start the creation of a separate sourcemap file as opposed to inline
-  .pipe(uglify())
   .pipe(sourcemaps.write('./')) // the calls to buffer and sourcemaps exist to make sure sourcemaps keep working
   .pipe(gulp.dest("dist"));
 });

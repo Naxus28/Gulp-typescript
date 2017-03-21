05a20992244cf0ddd77aaf8d8666e96b2ac5521f
Gabriel Ferraz <gferraz@fullsail.com>
2017-03-21 09:09:44 -0400

    watchifies browserified code

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
+var watchify = require("watchify");
 var tsify = require("tsify");
+var gutil = require("gulp-util");
 var paths = {
     pages: ['src/*.html']
 };
+
+// wraps browserify in watchify and cache the result
+var watchedBrowserify = watchify(
+   browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+  }).plugin(tsify)
+);
+
 
 // copies html to /dist
 gulp.task("copy-html", function () {
@@ -12,21 +26,20 @@ gulp.task("copy-html", function () {
     .pipe(gulp.dest("dist"));
 });
 
+// crates a function that bundles the watched browserify
+// this function will be called by the watchfied browserify on 'update'
+// and by the default task (so we bundle when we run 'gulp' for the first time)
+function bundle() {
+  return watchedBrowserify
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest("dist"));
+}
 
-// browserify the stream so we can render in the browser
-gulp.task("default", ["copy-html"], function () {
-  return browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/main.ts'],
-    cache: {},
-    packageCache: {}
-  })
-  .plugin(tsify)
-  .bundle()
-  .on('error', function (error) { console.error(error.toString()); })
-  .pipe(source('bundle.js'))
-  .pipe(gulp.dest("dist"));
-});
+gulp.task("default", ["copy-html"], bundle); // instead of registering the browserify task we call the 'bundle' function, which returns the watchified broweserify and bundles it
+watchedBrowserify.on("update", bundle); // Browserify will run the bundle function every time one of your TypeScript files changes
+watchedBrowserify.on("log", gutil.log); // log to the console
+
+
 
 
